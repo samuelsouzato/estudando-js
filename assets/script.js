3ff6908aa85eca6f70e47283e277b61083249ce8
Sam <samuel.souzato@gmail.com>
2025-05-26 01:48:27 -0300

    estudando js

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+let dolar = 5.1
+
+let usdInput = document.quertSelector("#usd")
+let brlInput = document.quertSelector("#brl")
+
+usdInput.addEventListener("keyup", () => {
+
+})
+
+brlInput.addEventListener("keyup", () => {
+    
+})
+
+usdInput.value = "1000,00"
+convert("usd-to-brl")
+
+//Funções
+
+function convert (type){
+    if(type == "usd-to-brl") {
+
+    }   
+    if(type == "brl-to-usd") {
+        
+    }  
+}
